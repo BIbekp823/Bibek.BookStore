43550f6e9892b7a87d00343bd374cb48a7fb6d78
Bibek Poudel <Bibekp823@gmail.com>
2024-12-10 11:52:23 +0545

    feat:Create Class for Book and Client

---
+++ src/Bibek.BookStore.Domain/Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace Bibek.BookStore.Model
+{
+    public  class Book : AggregateRoot<Guid>
+    {
+        
+            public string? Name { get; set; }
+            public string? Author { get; set; }
+            public DateTime IssueDate { get; set; }
+            public Guid ClientId { get; set; }
+            // Foreign key for Client
+            
+            //public Client Client { get; set; }
+
+    }
+}
--- src/Bibek.BookStore.Domain/Model/Client.cs
+++ src/Bibek.BookStore.Domain/Model/Client.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Bibek.BookStore.Model
 {
@@ -12,5 +13,7 @@ namespace Bibek.BookStore.Model
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Address { get; set; }
+        // Navigation property for related books
+        public virtual ICollection<Book> Books { get; set; } = new List<Book>();
     }
 }
