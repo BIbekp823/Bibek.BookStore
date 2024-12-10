23a82b0da1e262033f6e2ee23c9df408aec92b92
Bibek Poudel <Bibekp823@gmail.com>
2024-12-10 11:51:40 +0545

    feat:Interface for Book and Client

---
+++ src/Bibek.BookStore.Application.Contracts/Interface/IBookAppService.cs
@@ -0,0 +1,25 @@
+﻿using Bibek.BookStore.DTO;
+using Bibek.BookStore.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bibek.BookStore.Interface
+{
+    public  interface IBookAppService
+    {
+        Task<List<Book>> GetClientBooksAsync();
+        //Task<IActionResult> GetByIdAsync(Guid id);
+        //Task<IActionResult> CreateAsync(BookDTO input);
+        //Task<IActionResult> UpdateAsync(BookDTO book);
+        //Task<IActionResult> DeleteAsync(Guid Id);
+        //Task<IActionResult> CreateAsync(BookDTO input);
+
+        Task<IActionResult> CreateBookAsync(BookDTO input);
+        Task<IActionResult> UpdateBookAsync( Guid Id,BookDTO input);
+        Task DeleteBookAsync(Guid id);
+        Task<IActionResult> GetByIdAsync(Guid id);
+
+    }
+}
