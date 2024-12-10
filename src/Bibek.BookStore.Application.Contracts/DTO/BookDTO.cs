1ada16ed7df46d64fb5e137a09bb543ed0438f33
Bibek Poudel <Bibekp823@gmail.com>
2024-12-10 11:51:06 +0545

    feat:New DTO for Book and Client

---
+++ src/Bibek.BookStore.Application.Contracts/DTO/BookDTO.cs
@@ -0,0 +1,20 @@
+﻿using Bibek.BookStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace Bibek.BookStore.DTO
+{
+    public class BookDTO :EntityDto<Guid>
+    {
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public DateTime IssueDate { get; set; }
+        public Guid ClientId { get; set; }
+
+        //public Client Client { get; set; }
+    }
+}
--- src/Bibek.BookStore.Application.Contracts/DTO/ClientDTO.cs
+++ src/Bibek.BookStore.Application.Contracts/DTO/ClientDTO.cs
@@ -12,5 +12,7 @@ namespace Bibek.BookStore.DTO
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Address { get; set; }
+
+
     }
 }
